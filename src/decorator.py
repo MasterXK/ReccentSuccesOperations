71d1c039da59fe57f5b2b7a5fa093ab6a61924f2
vavil <vavilon164@yandex.ru>
2023-11-10 23:48:20 +0300

    Added decorator.py function

---
+++ src/decorator.py
@@ -0,0 +1,35 @@
+import time
+from functools import wraps
+from typing import Any, Callable
+
+
+def log(filename: str = None) -> Callable:
+    def wrapper(func: Callable) -> Any:
+        @wraps(func)
+        def inner(*args, **kwargs):
+            time_now = time.localtime()
+            response = time.strftime("%m-%d-%Y %H:%M:%S", time_now) + f' {func.__name__}'
+            try:
+                result = func(*args, **kwargs)
+                response += ' ok\n'
+            except Exception as e:
+                response += f' error: {e}. Inputs: {args, kwargs}\n'
+            finally:
+                if filename:
+                    with open(filename, 'a') as file:
+                        file.write(response)
+                    return
+                else:
+                    print(response)
+                    return
+        return inner
+    return wrapper
+
+
+if __name__ == '__main__':
+    @log()
+    def my_function(x, y):
+        return x + y
+
+
+    my_function(1, '2')
