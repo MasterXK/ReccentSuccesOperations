0601767fb7c8c0cb1e0c42bea0fdfedd02c09e58
vavil <vavilon164@yandex.ru>
2023-11-11 17:31:19 +0300

    New test_decorator.py

--- src/decorator.py
+++ src/decorator.py
@@ -4,24 +4,22 @@ from typing import Any, Callable
 
 
 def log(filename: str = None) -> Callable:
-    def wrapper(func: Callable) -> Any:
+    def wrapper(func: Callable) -> Callable:
         @wraps(func)
-        def inner(*args, **kwargs) -> Any:
+        def inner(*args: Any, **kwargs: Any) -> Any:
             time_now = time.localtime()
             response = time.strftime("%m-%d-%Y %H:%M:%S", time_now) + f' {func.__name__}'
             try:
                 result = func(*args, **kwargs)
                 response += ' ok'
             except Exception as e:
-                response += f' error: {e}. Inputs: {args, kwargs}'
+                response += f' error: {type(e).__name__}. Inputs: {args, kwargs}'
                 result = None
             if filename:
-                with open(filename, 'a') as file:
+                with open(filename, 'w') as file:
                     file.write(response + '\n')
-                return result
             else:
                 print(response)
-                return result
-
+            return result
         return inner
     return wrapper
--- tests/test_decorator.py
+++ tests/test_decorator.py
@@ -1,71 +1,53 @@
-import time
 import os
+import time
+
+import pytest
 
 from src.decorator import log
 
-FILENAME = os.path.join('.', 'data', 'Log.txt')
+TESTS_LOG = os.path.join('.', 'data', 'test.txt')
 
 
-def test_log_default_with_file():
-    @log(FILENAME)
+@pytest.mark.parametrize("arg_1, arg_2, expected_result",
+                         [(1, 2, ' my_function ok'),
+                          (1, '2',
+                           " my_function error: TypeError. Inputs: ((1, '2'), {})")])
+def test_log_with_file(arg_1, arg_2, expected_result):
+    if os.path.exists(TESTS_LOG):
+        os.remove(TESTS_LOG)
+
+    @log(TESTS_LOG)
     def my_function(x, y):
         return x + y
 
     time_ = time.localtime()
 
-    my_function(1, 2)
+    my_function(arg_1, arg_2)
+    expected_log = time.strftime("%m-%d-%Y %H:%M:%S", time_) + expected_result
+    with open(TESTS_LOG) as file:
+        log_message = file.read().strip()
+        assert log_message == expected_log
 
-    response = time.strftime("%m-%d-%Y %H:%M:%S", time_) + ' my_function ok\n'
-    with open(FILENAME) as file:
-        lines = file.readlines()
-        assert lines[-1] == response
 
+@pytest.mark.parametrize("arg_1, arg_2, expected_result",
+                         [(1, 2, ' my_function ok'),
+                          (1, '2',
+                           " my_function error: TypeError. Inputs: ((1, '2'), {})")])
+def test_log_default_without_file(capfd, arg_1, arg_2, expected_result):
+    if os.path.exists(TESTS_LOG):
+        os.remove(TESTS_LOG)
 
-def test_log_default_without_file(capfd):
     @log()
     def my_function(x, y):
         return x + y
 
     time_ = time.localtime()
 
-    my_function(1, 2)
+    my_function(arg_1, arg_2)
 
     captured = capfd.readouterr()
-    expect_response = time.strftime("%m-%d-%Y %H:%M:%S", time_) + ' my_function ok\n'
+    log_message = captured.out.strip()
+    expect_log = time.strftime("%m-%d-%Y %H:%M:%S", time_) + expected_result
 
-    assert captured.out == expect_response
+    assert log_message == expect_log
 
-
-def test_log_wrong_type_with_file():
-    @log(FILENAME)
-    def my_function(x, y):
-        return x + y
-
-    time_ = time.localtime()
-
-    my_function(1, '2')
-
-    expect_response = (time.strftime("%m-%d-%Y %H:%M:%S", time_) +
-                       " my_function error: unsupported operand type(s) for +: 'int' and 'str'. " +
-                       "Inputs: ((1, '2'), {})\n")
-
-    with open(FILENAME) as file:
-        lines = file.readlines()
-        assert lines[-1] == expect_response
-
-
-def test_log_wrong_type_without_file(capfd):
-    @log()
-    def my_function(x, y):
-        return x + y
-
-    time_ = time.localtime()
-
-    my_function(1, '2')
-
-    captured = capfd.readouterr()
-    expect_response = (time.strftime("%m-%d-%Y %H:%M:%S", time_) +
-                       " my_function error: unsupported operand type(s) for +: 'int' and 'str'. " +
-                       "Inputs: ((1, '2'), {})\n")
-
-    assert captured.out == expect_response
