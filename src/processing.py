b0ef82606a846e51bde12e5581e65aedbd8740e1
vavil <vavilon164@yandex.ru>
2023-11-24 19:57:01 +0300

    Added get_categories to processing.py

--- src/processing.py
+++ src/processing.py
@@ -22,5 +22,29 @@ def sort_by_date(operations: list[dict], reverse_: bool = False) -> list[dict]:
     return sorted(operations, key=lambda x: x["date"], reverse=reverse_)
 
 
-def get_categories(transactions: list[dict]) -> dict:
+def get_categories(transactions: list[dict], key_word: str) -> dict:
+    """
+    Функция разбивает транзакции по категориям со словом search_str
+    и считает количество транзакций в каждой полученной категории
+    :param transactions: список транзакций
+    :param key_word: ключевое слово
+    :return: словарь {категория: количество}
+    """
+    suitable_descriptions = []
+    pattern = re.compile(r'\b' + key_word.title() + r'\b')
 
+    for transaction in transactions:
+        try:
+            if re.search(pattern, transaction['description']) is None:
+                continue
+
+            else:
+                suitable_descriptions.append(transaction['description'])
+
+        except KeyError as e:
+
+            continue
+
+    result = collections.Counter(suitable_descriptions)
+
+    return dict(result)
