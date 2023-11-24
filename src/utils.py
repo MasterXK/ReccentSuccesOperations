312d62e9ac4fa48be0f1f279ab7d173e6a7598f5
vavil <vavilon164@yandex.ru>
2023-11-24 16:49:26 +0300

    Linter fixes

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,14 @@
 import json
 import os
-from pprint import pprint
 
 import pandas as pd
-from data import PATH_DATA
+
 from src.logger import setup_logger
 
 logger = setup_logger()
 
 
-def read_json(json_path: str) -> list[dict]:
+def read_json(json_path: str | os.PathLike) -> list[dict]:
     """
     Функция считывает содержимое json-файла
     :param json_path: пабсолютный путь до json-а
@@ -48,7 +47,12 @@ def get_sum(transaction: dict) -> float:
     raise ValueError("Транзация выполнена не в рублях. " "Укажите транзакцию в рублях.")
 
 
-def read_table(file_path: str) -> str | None:
+def read_table(file_path: str | os.PathLike) -> dict | None:
+    """
+    Функция считывает данные из тублиц .csv и .xlsx(.xls)
+    :param file_path: путь до файла с таблицей
+    :return: объект Python
+    """
     if not os.path.exists(file_path):
         logger.error("Файл не найден")
         return None
@@ -56,7 +60,7 @@ def read_table(file_path: str) -> str | None:
     _, ext = os.path.splitext(file_path)
 
     if ext == ".csv":
-        data = pd.read_csv(file_path, encoding='UTF-8', sep=';')
+        data = pd.read_csv(file_path, encoding="UTF-8", sep=";")
 
     elif ext in [".xls", ".xlsx"]:
         data = pd.read_excel(file_path)
@@ -66,28 +70,13 @@ def read_table(file_path: str) -> str | None:
         return None
 
     # operationAmount(currency( , ) + amount)
-    df_operation_amount = data.loc[:, ['amount', 'currency_name', 'currency_code']]
-    data.drop(['amount', 'currency_code', 'currency_name'], axis=1, inplace=True)
+    df_operation_amount = data.loc[:, ["amount", "currency_name", "currency_code"]]
+    data.drop(["amount", "currency_code", "currency_name"], axis=1, inplace=True)
 
-    return_data = json.loads(data.to_json(orient="records", force_ascii=False))
+    return_data: dict = json.loads(data.to_json(orient="records", force_ascii=False))
 
     for i in range(len(return_data)):
-
         amount, name, code = df_operation_amount.iloc[i]
-        return_data[i]['operationAmount'] = {'amount': amount, 'currency': {'name': name, 'code': code}}
+        return_data[i]["operationAmount"] = {"amount": amount, "currency": {"name": name, "code": code}}
 
     return return_data
-
-data_in = {
-        "amount": "0.0",
-        "currency_code": "_",
-        "currency_name": "_",
-        "date": "2023-09-05T11:30:32Z",
-        "description": "Перевод ",
-        "from": "Счет ",
-        "id": "0.0",
-        "state": "EXECUTED",
-        "to": "Счет ",
-    }
-df = pd.DataFrame(data_in, index=[0])
-print(df.loc[:, ['amount', 'currency_name', 'currency_code']])
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,8 @@
 import json
 import os
-import pandas as pd
-from unittest.mock import MagicMock, patch
+from unittest.mock import Mock, patch
 
+import pandas as pd
 import pytest
 
 from data import PATH_DATA
@@ -10,23 +10,23 @@ from src.utils import get_sum, read_json, read_table
 
 
 @pytest.fixture
-def transaction_rub():
+def transaction_rub() -> dict:
     with open(os.path.join(PATH_DATA, "test_2.json"), encoding="UTF-8") as json_file:
-        json_content = json.load(json_file)
+        json_content: dict = json.load(json_file)
 
     return json_content
 
 
 @pytest.fixture
-def transaction_usd():
+def transaction_usd() -> dict:
     with open(os.path.join(PATH_DATA, "test_1.json"), encoding="UTF-8") as json_file:
-        json_content = json.load(json_file)
+        json_content: list[dict] = json.load(json_file)
 
     return json_content[0]
 
 
 @pytest.fixture
-def data_in():
+def data_in() -> dict[str, str]:
     return {
         "amount": "0.0",
         "currency_code": "",
@@ -41,76 +41,79 @@ def data_in():
 
 
 @pytest.fixture
-def data_out():
-    return [{
-        "operationAmount": {"amount": "0.0", "currency": {"name": "", "code": ""}},
-        "date": "2023-09-05T11:30:32Z",
-        "description": "Перевод ",
-        "from": "Счет ",
-        "id": "0.0",
-        "state": "EXECUTED",
-        "to": "Счет ",
-    }]
+def data_out() -> list[dict]:
+    return [
+        {
+            "operationAmount": {"amount": "0.0", "currency": {"name": "", "code": ""}},
+            "date": "2023-09-05T11:30:32Z",
+            "description": "Перевод ",
+            "from": "Счет ",
+            "id": "0.0",
+            "state": "EXECUTED",
+            "to": "Счет ",
+        }
+    ]
 
 
 @pytest.mark.parametrize(
     "json_path, expected_result",
     [
         (
-                os.path.join(PATH_DATA, "test_1.json"),
-                [
-                    {
-                        "id": 41428829,
-                        "state": "EXECUTED",
-                        "date": "2019-07-03T18:35:29.512364",
-                        "operationAmount": {"amount": "8221.37", "currency": {"name": "USD", "code": "USD"}},
-                        "description": "Перевод организации",
-                        "from": "MasterCard 7158300734726758",
-                        "to": "Счет 35383033474447895560",
-                    }
-                ],
+            os.path.join(PATH_DATA, "test_1.json"),
+            [
+                {
+                    "id": 41428829,
+                    "state": "EXECUTED",
+                    "date": "2019-07-03T18:35:29.512364",
+                    "operationAmount": {"amount": "8221.37", "currency": {"name": "USD", "code": "USD"}},
+                    "description": "Перевод организации",
+                    "from": "MasterCard 7158300734726758",
+                    "to": "Счет 35383033474447895560",
+                }
+            ],
         ),
         (os.path.join(PATH_DATA, "test_2.json"), []),
         (os.path.join(PATH_DATA, "test_3.json"), []),
         (os.path.join(PATH_DATA, "test_4.json"), []),
     ],
 )
-def test_from_json(json_path, expected_result):
+def test_from_json(json_path: str | os.PathLike, expected_result: list[dict]) -> None:
     assert read_json(json_path) == expected_result
 
 
-def test_get_sum_correct(transaction_rub):
+def test_get_sum_correct(transaction_rub: dict) -> None:
     assert get_sum(transaction_rub) == 31957.58
 
 
-def test_get_sum_err(transaction_usd):
+def test_get_sum_err(transaction_usd: dict) -> None:
     with pytest.raises(ValueError):
         get_sum(transaction_usd)
 
 
-@patch("os.path")
+@patch("os.path.exists")
 @patch("pandas.read_csv")
 @patch("pandas.read_excel")
-def test_read_table(mock_read_excel, mock_read_csv, mock_path, data_in, data_out):
-    mock_path.exists.return_value = True
-    mock_path.splitext.return_value = ["", ".csv"]
+def test_read_table(
+    mock_read_excel: Mock,
+    mock_read_csv: Mock,
+    mock_exists: Mock,
+    data_in: dict[str, str],
+    data_out: list[dict],
+) -> None:
+    mock_exists.return_value = True
 
     df = pd.DataFrame(data_in, index=[0])
-
     mock_read_csv.return_value = df
 
     assert read_table("path.csv") == data_out
 
-    mock_path.exists.assert_called()
-    mock_path.splitext.assert_called()
+    mock_exists.assert_called_once()
     mock_read_csv.assert_called_once()
 
-    mock_path.splitext.return_value = ["", ".xls"]
     df = pd.DataFrame(data_in, index=[0])
     mock_read_excel.return_value = df
 
     assert read_table("path.xls") == data_out
 
-    mock_path.exists.assert_called()
-    mock_path.splitext.assert_called()
+    mock_exists.assert_called()
     mock_read_excel.assert_called_once()
